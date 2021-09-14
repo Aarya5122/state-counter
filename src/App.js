692d4acc368dd195343d693862b40d2c861b09bb
Aarya Nanndaann Singh M N <aarya.nanndaann.singh.m.n@gmail.com>
2021-09-14 22:06:33 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
+import "./App.css"
 
-function App() {
-  return (
+const App = () => {
+  
+  const [count /*As we are building a counter*/ ,setCount] = useState(0 /* Inital value of count */); // Using states
+  /*
+  life setLife
+  salary setSalary
+  */ 
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Counter App Using States/Hooks</h1>
       </header>
+      <h2>Current value of count is: {count}</h2>
+      <button onClick={() => (setCount(0))}>Reset Counter</button>
+      <button onClick={()=>(count>=10? "" : setCount(count+1))}>Increase Counter</button>
+      <button onClick={() => (count<=0? "" : setCount(count-1))}>Decrease Counter</button>
     </div>
   );
 }
